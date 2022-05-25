2f7e1b599ad096d2cd6ed69e88952a9b29eb6500
mada-droid <75455554+mada-droid@users.noreply.github.com>
2022-05-25 11:38:04 +0200

    Delete V2/demo directory

--- V2/demo/src/main/java/com/example/demo/dao/UtenteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.entity.Utente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UtenteRepository extends JpaRepository<Utente, Integer> {
-}
