de243c730d049c657f581c5e59bb5efc0e46c5b3
mada-droid <75455554+mada-droid@users.noreply.github.com>
2022-05-27 17:19:16 +0200

    Delete authentication/demo/src directory

--- authentication/demo/src/main/java/com/example/demo/dao/ContoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.entity.Conto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContoRepository extends JpaRepository<Conto, Integer> {
-}
