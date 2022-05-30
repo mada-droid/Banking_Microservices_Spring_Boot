94faa2416b4858aac835a9f97c8f2db9ecce265a
mada-droid <75455554+mada-droid@users.noreply.github.com>
2022-05-30 15:44:54 +0200

    Add files via upload

---
+++ authentication/demo/src/main/java/com/example/demo/dao/UtenteRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.Utente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UtenteRepository extends JpaRepository<Utente, Integer> {
+//    Utente findByUsername(String userName);
+    Utente findByEmail(String email);
+
+}
