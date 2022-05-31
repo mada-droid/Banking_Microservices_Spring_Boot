a53d67925a1ea77d9403db78da60b16ef30760ce
mada-droid <75455554+mada-droid@users.noreply.github.com>
2022-05-31 12:40:54 +0200

    Delete account-microservice-ML/src directory

--- account-microservice-ML/src/main/java/com/madalore/accountmicroserviceML/dao/ContoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.madalore.accountmicroserviceML.dao;
-
-import com.madalore.accountmicroserviceML.entity.Conto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContoRepository extends JpaRepository<Conto, Integer> {
-}
--- account-microservice-ML/src/main/java/com/madalore/accountmicroserviceML/dao/UtenteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.madalore.accountmicroserviceML.dao;
-
-
-import com.madalore.accountmicroserviceML.entity.Utente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UtenteRepository extends JpaRepository<Utente, Integer> {
-//    Utente findByUsername(String userName);
-}
