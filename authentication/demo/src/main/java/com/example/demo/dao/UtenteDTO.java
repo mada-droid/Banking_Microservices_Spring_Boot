d52fe8a3953f4ff1e298dd9391d491a299a37e72
mada-droid <75455554+mada-droid@users.noreply.github.com>
2022-05-31 12:40:11 +0200

    Add files via upload

---
+++ authentication/demo/src/main/java/com/example/demo/dao/UtenteDTO.java
@@ -0,0 +1,51 @@
+package com.example.demo.dao;
+
+public class UtenteDTO {
+
+//    private String username;
+    private String password;
+    private String birthDate;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
