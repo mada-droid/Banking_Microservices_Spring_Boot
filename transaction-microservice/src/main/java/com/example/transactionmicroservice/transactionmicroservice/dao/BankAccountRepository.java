e89499c2c3283f27695fa7539c63c6f6cd875d59
mada-droid <75455554+mada-droid@users.noreply.github.com>
2022-06-09 17:43:29 +0200

    Delete transaction-microservice/src directory

--- transaction-microservice/src/main/java/com/example/transactionmicroservice/transactionmicroservice/dao/BankAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.transactionmicroservice.transactionmicroservice.dao;
-
-import com.example.transactionmicroservice.transactionmicroservice.entity.BankAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BankAccountRepository extends JpaRepository<BankAccount, Integer> {
-}
