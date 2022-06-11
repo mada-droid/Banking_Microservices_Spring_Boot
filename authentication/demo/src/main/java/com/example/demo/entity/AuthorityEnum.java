0fe16857d5c220498586cf6f17dd1f1dd0a98cff
mada-droid <75455554+mada-droid@users.noreply.github.com>
2022-06-11 10:44:37 +0200

    Add files via upload

---
+++ authentication/demo/src/main/java/com/example/demo/entity/AuthorityEnum.java
@@ -0,0 +1,7 @@
+package com.example.demo.entity;
+
+
+public enum AuthorityEnum {
+    ROLE_CLIENT,
+    ROLE_EMPLOYEE
+}
---
+++ authentication/demo/src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -0,0 +1,81 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.dao.UserRepository;
+import com.example.demo.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<UserDTO> findAll() {
+
+        return userRepository.findAll().stream()
+                .map(x -> new UserDTO(x.getPassword(),
+                        x.getBirthDate(),x.getFirstName(),
+                        x.getLastName(),x.getEmail())).collect(Collectors.toList());
+
+    }
+
+    @Override
+    public UserDTO findById(int theId) {
+        Optional<User> result = userRepository.findById(theId);
+
+        User user = null;
+
+        if (result.isPresent()) {
+            user = result.get();
+        }
+        else {
+            // we didn't find the employee
+            throw new RuntimeException("Utente con id: " + theId + " non trovato");
+        }
+
+        UserDTO userDTO = new UserDTO(user.getPassword(),
+                user.getBirthDate(), user.getFirstName(),
+                user.getLastName(), user.getEmail());
+
+        return userDTO;
+    }
+
+    @Override
+    public void save(UserDTO userDTO) {
+
+        User user = new User(userDTO.getFirstName(), userDTO.getLastName(),
+                userDTO.getEmail(), userDTO.getBirthDate(), userDTO.getPassword());
+
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        userRepository.deleteById(theId);
+    }
+
+    @Override
+    public UserDTO findByEmail(String email) {
+        User user =  userRepository.findByEmail(email);
+
+        UserDTO userDTO = new UserDTO(user.getPassword(),
+                user.getBirthDate(), user.getFirstName(), user.getLastName(),
+                user.getEmail());
+
+        userDTO.setId(user.getId());
+
+        return userDTO;
+    }
+}
