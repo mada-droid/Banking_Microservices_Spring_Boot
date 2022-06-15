d4201c7f250fe24d1c5be9787415453d8d121b45
mada-droid <75455554+mada-droid@users.noreply.github.com>
2022-06-15 16:43:55 +0200

    Add files via upload

---
+++ apiGateway_v2/apiGateway/src/main/java/com/example/springcloud/gateway/apiGateway/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,139 @@
+package com.example.springcloud.gateway.apiGateway.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.example.springcloud.gateway.apiGateway.exception.JwtTokenMalformedException;
+import com.example.springcloud.gateway.apiGateway.exception.JwtTokenMissingException;
+import com.example.springcloud.gateway.apiGateway.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import io.jsonwebtoken.Claims;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthenticationFilter implements GatewayFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = (ServerHttpRequest) exchange.getRequest();
+
+        final List<String> apiEndpoints = List.of("/register", "/auth", "/userdetails");
+
+        Predicate<ServerHttpRequest> isApiSecured = r -> apiEndpoints.stream()
+                .noneMatch(uri -> r.getURI().getPath().contains(uri));
+
+        if (isApiSecured.test(request)) {
+            if (!request.getHeaders().containsKey("Authorization")) {
+                ServerHttpResponse response = exchange.getResponse();
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                return response.setComplete();
+            }
+
+            final String token = request.getHeaders().getOrEmpty("Authorization").get(0).substring(7);
+
+            try {
+                jwtUtil.validateToken(token);
+            } catch (JwtTokenMalformedException | JwtTokenMissingException e) {
+                // e.printStackTrace();
+
+                ServerHttpResponse response = exchange.getResponse();
+                response.setStatusCode(HttpStatus.BAD_REQUEST);
+
+                return response.setComplete();
+            }
+
+            String urlToCheck = request.getURI().toString();
+            String numberToPass = "";
+            for (int i = 21; i < urlToCheck.length(); i++) {
+                if (urlToCheck.charAt(i) >= 48 && urlToCheck.charAt(i) <= 57) {
+                    numberToPass += urlToCheck.charAt(i);
+                    if (i < urlToCheck.length() - 1) {
+                        if (urlToCheck.charAt(i + 1) < 48 || urlToCheck.charAt(i + 1) > 57) {
+                            numberToPass += ",";
+                        }
+                    }
+                }
+            }
+
+            ArrayList<String> stringArrayList = new ArrayList<>(List.of(numberToPass.split(",")));
+            if(stringArrayList.size()<3){
+                stringArrayList.add("1");
+                stringArrayList.add("1");
+            }
+
+            Claims claims = jwtUtil.getClaims(token);
+            final List<String> apiEndpointsEmployee = List.of("/api/auth/users/" + stringArrayList.get(0) + "/delete",
+                    "api/auth/users", "api/auth/users/" + stringArrayList.get(0),
+                    "/api/transaction/" + stringArrayList.get(0) + "/transactions",
+                    "/api/transaction/all_operations",
+                    "/api/account/users/" + stringArrayList.get(0) + "/all_accounts",
+                    "/api/account/accounts/requests",
+                    "/api/account/accounts/" + stringArrayList.get(0),
+                    "/api/account/accounts/activation/" + stringArrayList.get(0));
+            final List<String> apiEndpointsClient = List.of("/api/account/users/" + stringArrayList.get(0) + "/accounts",
+                    "/api/transaction/transaction", "/api/transaction/transfer",
+                    "/api/transaction/last_ten_operation/" + stringArrayList.get(0),
+                    "/api/transaction/last_twenty_operation/" + stringArrayList.get(0),
+                    "/api/transaction/last_fifty_operation/" + stringArrayList.get(0),
+                    "/api/account/users/" + stringArrayList.get(0) + "/accounts",
+                    "/api/transaction/" + stringArrayList.get(0) + "/transactions",
+                    "/api/account/accounts/closure_request/" + stringArrayList.get(0),
+                    "/api/account/accounts/activation_request/" + stringArrayList.get(0),
+                    "/api/account/"+stringArrayList.get(0)+"/addAccount/"+stringArrayList.get(1)+"/"+stringArrayList.get(2),
+                    "api/auth/users/update");
+
+            isApiSecured = r -> apiEndpointsClient.stream()
+                    .anyMatch(uri -> r.getURI().getPath().contains(uri));
+
+            for (String s : apiEndpointsClient) {
+                String control = "http://localhost:8080" + s;
+                if (control.equals(urlToCheck)) {
+                    if (isApiSecured.test(request)) {
+                        if (!((ArrayList<String>) claims.get("authorities")).contains("ROLE_CLIENT")) {
+                            ServerHttpResponse response = exchange.getResponse();
+                            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                            return response.setComplete();
+                        }
+                    }
+                    exchange.getRequest().mutate().header("id", String.valueOf(claims.get("id"))).build();
+                }
+            }
+
+            isApiSecured = r -> apiEndpointsEmployee.stream()
+                    .anyMatch(uri -> r.getURI().getPath().contains(uri));
+
+
+            for (String s : apiEndpointsEmployee) {
+                String control = "http://localhost:8080" + s;
+                if (control.equals(urlToCheck)) {
+                    if (isApiSecured.test(request)) {
+                        if (!((ArrayList<String>) claims.get("authorities")).contains("ROLE_EMPLOYEE")) {
+                            ServerHttpResponse response = exchange.getResponse();
+                            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                            return response.setComplete();
+                        }
+                    }
+                    exchange.getRequest().mutate().header("id", String.valueOf(claims.get("id"))).build();
+                }
+            }
+
+        }
+
+        return chain.filter(exchange);
+    }
+
+}
