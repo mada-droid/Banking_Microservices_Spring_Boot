3fc87574f386d74dc6a1112fd5ccd3d28a414c11
mada-droid <75455554+mada-droid@users.noreply.github.com>
2022-06-20 17:37:39 +0200

    Add files via upload

---
+++ account-microservice-ML/src/main/java/com/madalore/accountmicroserviceML/service/AmountMessageListener.java
@@ -0,0 +1,76 @@
+package com.madalore.accountmicroserviceML.service;
+
+import com.madalore.accountmicroserviceML.dto.*;
+import com.madalore.accountmicroserviceML.entity.State;
+import com.madalore.accountmicroserviceML.utils.BankAccountUtils;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AmountMessageListener {
+
+    BankAccountService bankAccountService;
+
+    public AmountMessageListener(BankAccountService bankAccountService) {
+        this.bankAccountService = bankAccountService;
+    }
+
+    @RabbitListener(queues = "myQueue")
+    public Boolean receive(AmountMessageDTO amountMessageDTO) {
+
+        BankAccountDTO bankAccountDTO = bankAccountService.findBankAccountById(amountMessageDTO.getAccountId());
+
+        if (bankAccountDTO.getBalance() + amountMessageDTO.getAmount() >= 0 && bankAccountDTO.getState().equals(State.ACTIVE)) {
+            bankAccountDTO.setBalance(bankAccountDTO.getBalance() + amountMessageDTO.getAmount());
+            bankAccountService.update(bankAccountDTO);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @RabbitListener(queues = "newAccountQueue")
+    public Boolean receive(NewAccountMessageDTO newAccountMessageDTO) {
+        try {
+            BankAccountUtils bankAccountUtils = new BankAccountUtils();
+
+            BankAccountDTO bankAccountDTO = new BankAccountDTO(bankAccountUtils.generateIban(),
+                    State.INACTIVE, 0.00, newAccountMessageDTO.getUserId(), bankAccountUtils.getAccountNumber());
+
+            bankAccountService.save(bankAccountDTO);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
+    @RabbitListener(queues = "transferQueue")
+    public Object receiveTransfer(TransferMessageDTO transferMessageDTO) {
+
+        try {
+            BankAccountDTO bankAccountDTOFrom = bankAccountService.findBankAccountByIban(transferMessageDTO.getOriginIban());
+            BankAccountDTO bankAccountDTOTo = bankAccountService.findBankAccountByIban(transferMessageDTO.getDestinationIban());
+
+            if (bankAccountDTOFrom.getBalance() - transferMessageDTO.getAmount() >= 0
+                    && bankAccountDTOFrom.getState().equals(State.ACTIVE)
+                    && bankAccountDTOTo.getState().equals(State.ACTIVE)
+                    && transferMessageDTO.getAmount() > 0) {
+                bankAccountDTOFrom.setBalance(bankAccountDTOFrom.getBalance() - transferMessageDTO.getAmount());
+                bankAccountDTOTo.setBalance(bankAccountDTOTo.getBalance() + transferMessageDTO.getAmount());
+                bankAccountService.update(bankAccountDTOFrom);
+                bankAccountService.update(bankAccountDTOTo);
+                TransferResponseDTO transferResponseDTO =
+                        new TransferResponseDTO(bankAccountDTOFrom.getId(),bankAccountDTOTo.getId());
+                return  transferResponseDTO;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
+
+
---
+++ account-microservice-ML/src/main/java/com/madalore/accountmicroserviceML/service/BankAccountServiceImpl.java
@@ -0,0 +1,138 @@
+package com.madalore.accountmicroserviceML.service;
+
+import com.madalore.accountmicroserviceML.dto.BankAccountDTO;
+import com.madalore.accountmicroserviceML.dao.BankAccountRepository;
+import com.madalore.accountmicroserviceML.entity.BankAccount;
+import com.madalore.accountmicroserviceML.entity.State;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BankAccountServiceImpl implements BankAccountService {
+
+    private BankAccountRepository bankAccountRepository;
+
+
+    @Autowired
+    public BankAccountServiceImpl(BankAccountRepository bankAccountRepository) {
+        this.bankAccountRepository = bankAccountRepository;
+    }
+
+    @Override
+    public void save(BankAccountDTO bankAccountDTO) {
+
+        BankAccount bankAccount = new BankAccount(bankAccountDTO.getAccountNumber(),
+        bankAccountDTO.getIban(),bankAccountDTO.getState(),bankAccountDTO.getBalance(),
+        bankAccountDTO.getUserId());
+        bankAccount.setId(0);
+
+        bankAccountRepository.save(bankAccount);
+    }
+
+    public void update(BankAccountDTO bankAccountDTO) {
+
+        BankAccount bankAccount = new BankAccount(bankAccountDTO.getAccountNumber(),
+                bankAccountDTO.getIban(),bankAccountDTO.getState(),bankAccountDTO.getBalance(),
+                bankAccountDTO.getUserId());
+        bankAccount.setId(bankAccountDTO.getId());
+
+        bankAccountRepository.save(bankAccount);
+    }
+
+    @Override
+    public List<BankAccountDTO> findAllByRequest() {
+
+        List<BankAccountDTO> bankAccountDTOSList = new ArrayList<>();
+        List<BankAccount> bankAccountList = bankAccountRepository.findAll();
+
+        for (BankAccount bankAccount : bankAccountList) {
+            if((bankAccount.getState().equals(State.CLOSURE_REQUEST))
+                    ||bankAccount.getState().equals(State.ACTIVATION_REQUEST)){
+                BankAccountDTO bankAccountDTO = new BankAccountDTO(bankAccount.getIban(),bankAccount.getState(),
+                        bankAccount.getBalance(),bankAccount.getUserId(),bankAccount.getAccountNumber());
+                bankAccountDTO.setId(bankAccount.getId());
+                bankAccountDTOSList.add(bankAccountDTO);
+            }
+        }
+        if(bankAccountDTOSList.size() > 0){
+            return bankAccountDTOSList;
+        }else{
+            throw new RuntimeException("Non ci sono richieste al momento !");
+        }
+    }
+
+    @Override
+    public BankAccountDTO findBankAccountByAccountNumber(String accountNumber) {
+
+        BankAccount bankAccount = bankAccountRepository.findBankAccountByAccountNumber(accountNumber);
+
+        BankAccountDTO bankAccountDTO = new BankAccountDTO(bankAccount.getIban(),bankAccount.getState(),
+                bankAccount.getBalance(),bankAccount.getUserId(),bankAccount.getAccountNumber());
+        bankAccountDTO.setId(bankAccount.getId());
+
+        return bankAccountDTO;
+    }
+
+    @Override
+    public BankAccountDTO findBankAccountByUserId(int userId) {
+        BankAccount bankAccount = bankAccountRepository.findBankAccountByUserId(userId);
+        BankAccountDTO bankAccountDTO = new BankAccountDTO(bankAccount.getIban(),
+                bankAccount.getState(),bankAccount.getBalance(),bankAccount.getUserId(),bankAccount.getAccountNumber());
+        bankAccountDTO.setUserId(userId);
+        return bankAccountDTO;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        bankAccountRepository.deleteById(theId);
+    }
+
+    public BankAccountDTO findBankAccountById(int accountId) {
+
+        BankAccount bankAccount = bankAccountRepository.findBankAccountById(accountId);
+
+        BankAccountDTO bankAccountDTO = new BankAccountDTO(bankAccount.getIban(),bankAccount.getState(),
+                bankAccount.getBalance(),bankAccount.getUserId(),bankAccount.getAccountNumber());
+        bankAccountDTO.setId(bankAccount.getId());
+
+        return bankAccountDTO;
+    }
+
+    private List<BankAccountDTO> getAccountDTOSConversion(List<BankAccount> bankAccountList) {
+        List<BankAccountDTO> bankAccountDTOList = new ArrayList<>();
+        BankAccountDTO bankAccountDTO;
+
+        for (BankAccount bankAccount : bankAccountList) {
+
+            bankAccountDTO = new BankAccountDTO(bankAccount.getIban(),
+                    bankAccount.getState(), bankAccount.getBalance(), bankAccount.getUserId(),
+                    bankAccount.getAccountNumber());
+            bankAccountDTO.setId(bankAccount.getId());
+
+            bankAccountDTOList.add(bankAccountDTO);
+        }
+
+        return bankAccountDTOList;
+    }
+
+    public List<BankAccountDTO> findAllByUserId(int userId){
+        List<BankAccount> bankAccountList = bankAccountRepository.findAllByUserId(userId);
+        return getAccountDTOSConversion(bankAccountList);
+    }
+
+    public BankAccountDTO findBankAccountByIban(String iban){
+
+        BankAccount bankAccount = bankAccountRepository.findBankAccountByIban(iban);
+
+        BankAccountDTO bankAccountDTO = new BankAccountDTO(bankAccount.getIban(),bankAccount.getState(),
+                bankAccount.getBalance(),bankAccount.getUserId(),bankAccount.getAccountNumber());
+        bankAccountDTO.setId(bankAccount.getId());
+
+        return bankAccountDTO;
+
+    }
+
+}
