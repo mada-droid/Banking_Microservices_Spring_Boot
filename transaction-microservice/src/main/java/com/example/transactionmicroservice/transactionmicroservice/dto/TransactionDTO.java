f436bc6a2856bec9b67eaedc8789fdbaab8671ae
mada-droid <75455554+mada-droid@users.noreply.github.com>
2022-06-22 17:52:51 +0200

    Add files via upload

---
+++ transaction-microservice/src/main/java/com/example/transactionmicroservice/transactionmicroservice/dto/TransactionDTO.java
@@ -0,0 +1,74 @@
+package com.example.transactionmicroservice.transactionmicroservice.dto;
+
+import com.example.transactionmicroservice.transactionmicroservice.entity.OperationType;
+
+import java.util.Date;
+
+public class TransactionDTO {
+
+    private int id;
+    private OperationType operationType;
+    private double amount;
+    private Date data;
+    private int accountId;
+    private String causal;
+
+
+    public TransactionDTO(){}
+
+    public TransactionDTO(OperationType operationType, double amount, Date data, int accountId, String causal) {
+        this.operationType = operationType;
+        this.amount = amount;
+        this.data = data;
+        this.accountId = accountId;
+        this.causal = causal;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public OperationType getOperationType() {
+        return operationType;
+    }
+
+    public void setOperationType(OperationType operationType) {
+        this.operationType = operationType;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getCausal() {
+        return causal;
+    }
+
+    public void setCausal(String causal) {
+        this.causal = causal;
+    }
+}
