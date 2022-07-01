1dd3438034321b07604002d3f0329557fc25b871
mada-droid <75455554+mada-droid@users.noreply.github.com>
2022-07-01 09:10:11 +0200

    Add files via upload

---
+++ account-microservice-ML/src/test/java/com/madalore/accountmicroserviceML/AccountMicroserviceMlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madalore.accountmicroserviceML;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountMicroserviceMlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
