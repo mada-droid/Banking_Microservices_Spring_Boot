ed379e3e3ccbc4a659386e2d02a206a69c643223
mada-droid <75455554+mada-droid@users.noreply.github.com>
2022-07-06 10:53:08 +0200

    Add files via upload

---
+++ authentication/demo/src/main/java/com/example/demo/dto/UserDTO.java
@@ -0,0 +1,69 @@
+package com.example.demo.dto;
+
+import java.util.Date;
+
+public class UserDTO {
+
+    private String password;
+    private Date birthDate;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int id;
+
+    public UserDTO(String password, Date birthDate, String firstName, String lastName, String email) {
+        this.password = password;
+        this.birthDate = birthDate;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
